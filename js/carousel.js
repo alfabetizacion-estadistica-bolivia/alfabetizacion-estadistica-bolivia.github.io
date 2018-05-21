4e3970a487e59e5f35cc7357eadde6db89377a0d
Beatriz Mamani <37555831+BiaM2@users.noreply.github.com>
2018-05-21 11:10:55 -0400

    logos

--- js/carousel.js
+++ js/carousel.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
   $('.customer-logos').slick({
-    slidesToShow: 6,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 3000,
     arrows: false,
     dots: false,
     pauseOnHover: false,
     responsive: [{
       breakpoint: 768,
       settings: {
-        slidesToShow: 4
+        slidesToShow: 3
       }
     }, {
       breakpoint: 520,
       settings: {
-        slidesToShow: 3
+        slidesToShow: 2
       }
     }]
   });
